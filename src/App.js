3ff80ce9399bf12ed171038a0980efb3f16c1313
Joshua <voydik@gmail.com>
2023-08-03 21:35:52 -0700

    C to F because America

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     const cities = ['Portland', 'San Diego', 'Charleston', 'Joshua Tree'];
     const requests = cities.map((city) =>
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=21d997e87f1d53224fc52c751a8d3e83`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=21d997e87f1d53224fc52c751a8d3e83`)
     );
 
     Promise.all(requests)
@@ -21,7 +21,7 @@ function App() {
       {weatherData.map((data, index) => (
         <div key={index} className="weather rounded-xl bg-blue-800 p-4 text-white shadow-lg min-w-full min-h-full bg-opacity-90 flex flex-col justify-center items-center hover:bg-blue-900 transition-all duration-200">
           <h2 className="text-6xl font-semibold">{data.name}</h2>
-          {data.main && <p className="text-2xl">{Math.round((data.main.temp - 32) * 5/9)}°C</p>}
+          {data.main && <p className="text-2xl">{Math.round(data.main.temp)}°F</p>}
           {data.weather && data.weather[0] && <p className="capitalize text-2xl">{data.weather[0].description}</p>}
         </div>
       ))}
